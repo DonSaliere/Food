e53416738c2b89c2684be7c4026d1422e1ebccb7
Andrey <s.adnrey-klg@yandex.ru>
2020-10-06 19:26:45 +0300

    async/away

--- js/script.js
+++ js/script.js
@@ -139,7 +139,6 @@ window.addEventListener('DOMContentLoaded', () => {
 
 	window.addEventListener('scroll', showModalbyScroll);
 
-
 	//Class
 
 	class MenuCard {
@@ -183,36 +182,23 @@ window.addEventListener('DOMContentLoaded', () => {
 		}
 	}
 
-	new MenuCard(
-		"img/tabs/vegy.jpg",
-		"vegy",
-		'Меню "Фитнес"',
-		'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов.Продукт активных и здоровых людей.Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
-		9,
-		'.menu .container',
-		"menu__item",
-		"big"
-	).render();
+	const getResource = async (url) => {
+		const res = await fetch(url);
 
-	new MenuCard(
-		"img/tabs/elite.jpg",
-		"elite",
-		'Меню “Премиум”',
-		'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд.Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
-		14,
-		'.menu .container',
-		"menu__item"
-	).render();
+		if (!res.ok) {
+			throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+		}
 
-	new MenuCard(
-		"img/tabs/post.jpg",
-		"post",
-		'Меню "Постное"',
-		'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
-		21,
-		'.menu .container',
-		"menu__item"
-	).render();
+		return await res.json();
+
+	};
+
+	getResource('http://localhost:3000/menu')
+		.then(data => {
+			data.forEach(({ img, altimg, title, descr, price }) => {
+				new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
+			});
+		});
 
 	//Forms
 
@@ -225,10 +211,23 @@ window.addEventListener('DOMContentLoaded', () => {
 	};
 
 	forms.forEach(item => {
-		postData(item);
+		bindpostData(item);
 	});
 
-	function postData(form) {
+	const postData = async (url, data) => {
+		const res = await fetch(url, {
+			method: "POST",
+			headers: {
+				'Content-type': 'application/json'
+			},
+			body: data
+		});
+
+		return await res.json();
+
+	};
+
+	function bindpostData(form) {
 		form.addEventListener('submit', (e) => {
 			e.preventDefault();
 
@@ -240,29 +239,10 @@ window.addEventListener('DOMContentLoaded', () => {
 			`;
 			form.insertAdjacentElement('afterend', statusMessage);
 
-			// const request = new XMLHttpRequest();
-			// request.open('POST', 'js/server.php');
-			// request.setRequestHeader('Content-type', 'application/json');
-
 			const formData = new FormData(form);
+			const json = JSON.stringify(Object.fromEntries(formData.entries()));
 
-			const object = {};
-			formData.forEach(function (value, key) {
-				object[key] = value;
-			});
-
-			// const json = JSON.stringify(object);
-
-			// request.send(formData);
-			// request.send(json);
-
-			fetch('js/server.php', {
-				method: "POST",
-				// headers: {
-				// 	'Content-type': 'application/json'
-				// },
-				body: JSON.stringify(object)
-			}).then(data => data.text())
+			postData('http://localhost:3000/requests', json)
 				.then(data => {
 					console.log(data);
 					showThanksModal(message.success);
@@ -272,17 +252,6 @@ window.addEventListener('DOMContentLoaded', () => {
 				}).finally(() => {
 					form.reset(); //очищаем форму			
 				});
-
-			// request.addEventListener('load', () => {
-			// 	if (request.status === 200) {
-			// 		console.log(request.response);
-			// 		showThanksModal(message.success);
-			// 		form.reset(); //очищаем форму			
-			// 		statusMessage.remove(); // удаляем текст
-			// 	} else {
-			// 		showThanksModal(message.failure);
-			// 	}
-			// });
 		});
 	}
 
@@ -309,9 +278,4 @@ window.addEventListener('DOMContentLoaded', () => {
 			closeModal();
 		}, 6000);
 	}
-
-	fetch('http://localhost:3000/menu')
-		.then(data => data.json())
-		.then(res => console.log(res));
-
 });
