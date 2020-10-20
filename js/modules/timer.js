b12982f3db83c337c00644ba30d5a50f46a31a42
Andrey <s.adnrey-klg@yandex.ru>
2020-10-20 14:49:01 +0300

    add Webpack

---
+++ js/modules/timer.js
@@ -0,0 +1,57 @@
+function timer() {
+	// Timer
+	const dedline = '2020-10-23';
+
+	function getTimeRemaining(endtime) {
+		const t = Date.parse(endtime) - Date.parse(new Date()),
+			days = Math.floor(t / (1000 * 60 * 60 * 24)),
+			hours = Math.floor((t / (1000 * 60 * 60) % 24)),
+			minutes = Math.floor((t / 1000 / 60) % 60),
+			second = Math.floor((t / 1000) % 60);
+
+		return {
+			'total': t,
+			'days': days,
+			'hours': hours,
+			'minutes': minutes,
+			'second': second
+		};
+
+	}
+
+	function getZero(num) {
+		if (num >= 0 && num < 10) {
+			return `0${num}`;
+		} else {
+			return num;
+		}
+	}
+
+	function setClock(selector, endtime) {
+		const timer = document.querySelector(selector),
+			days = timer.querySelector('#days'),
+			hours = timer.querySelector('#hours'),
+			minutes = timer.querySelector('#minutes'),
+			second = timer.querySelector('#seconds'),
+			timInterval = setInterval(updateClock, 1000);
+
+		updateClock();
+
+		function updateClock() {
+			const t = getTimeRemaining(endtime);
+
+			days.innerHTML = getZero(t.days);
+			hours.innerHTML = getZero(t.hours);
+			minutes.innerHTML = getZero(t.minutes);
+			second.innerHTML = getZero(t.second);
+
+			if (t.total <= 0) {
+				clearInterval(timInterval);
+			}
+		}
+	}
+
+	setClock('.timer', dedline);
+}
+
+module.exports = timer;
